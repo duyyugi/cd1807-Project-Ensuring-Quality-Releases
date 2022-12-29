2e161d18391784a10ab57c7c1cbe61d329b4c8ec
duyyugi <quocduy10a1@gmail.com>
2022-12-29 22:28:00 +0700

    update yaml

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+from selenium.webdriver.common.by import By
+
 
 # Start the browser and login with standard_user
 def login (user, password):
@@ -14,12 +16,11 @@ def login (user, password):
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
-    driver.find_elements(By.ID, "user-name").send_keys(user)
-    driver.find_elements(By.ID, "password").send_keys(password)
-    driver.find_elements(By.ID, "login-button").click()
-
-    link = driver.find_element(By.ID, "item_4_img_link").src
-    assert "/static/media/sauce-backpack-1200x1500.34e7aa42.jpg" in link
+    driver.find_element(By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
+    driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
+    product_label = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_item_name']").text
+    assert "Sauce Labs Backpack" in product_label
 
 login('standard_user', 'secret_sauce')
 
