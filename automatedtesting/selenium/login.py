5c8a6ef5a5e4397d8fd02b181c73e0e82ae8558d
duyyugi <quocduy10a1@gmail.com>
2022-12-29 22:12:34 +0700

    update yaml

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -14,5 +14,12 @@ def login (user, password):
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
+    driver.find_elements(By.ID, "user-name").send_keys(user)
+    driver.find_elements(By.ID, "password").send_keys(password)
+    driver.find_elements(By.ID, "login-button").click()
+
+    link = driver.find_element(By.ID, "item_4_img_link").src
+    assert "/static/media/sauce-backpack-1200x1500.34e7aa42.jpg" in link
+
 login('standard_user', 'secret_sauce')
 
