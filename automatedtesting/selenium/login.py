637e4aef6508bb338d5a224d77ad29157e73268c
duyyugi <quocduy10a1@gmail.com>
2022-12-30 12:44:00 +0700

    add timestamp

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -3,18 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 import syslog
+import datetime
 
+
+def timestamp():
+    ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return (ts + '\t')
 
 # Start the browser and login with standard_user
 def login (user, password):
-    print ('Starting the browser...')
+    print (timestamp() + 'Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--headless') 
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print (timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     syslog.syslog('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
@@ -25,7 +30,7 @@ def login (user, password):
     driver.find_element(By.ID, "login-button").click()
 
     assert driver.current_url == homepage_url
-    print ('user ' + user + ' login test successfully')
+    print (timestamp() + 'user ' + user + ' login test successfully')
     syslog.syslog('user ' + user + ' login test successfully')
 
     return driver
@@ -35,17 +40,17 @@ def add_and_remove_items_to_cart(driver):
     add_button_lists = driver.find_elements(By.CLASS_NAME, "btn_primary btn_inventory")
     add_button_lists = driver.find_elements(By.CLASS_NAME, "btn_secondary btn_inventory")
     for item in items:
-        print (item.text + "is added to cart")
+        print (timestamp() + item.text + "is added to cart")
         syslog.syslog(item.text + ' login test successfully')
     for button in add_button_lists:
         button.click()
     for item in items:
-        print (item.text + "is removed from cart")
+        print (timestamp() + item.text + "is removed from cart")
         syslog.syslog(item.text + "is removed from cart")
     for button in add_button_lists:
         button.click()
     
-    print ('add and remove test successfully')
+    print (timestamp() + 'add and remove test successfully')
     syslog.syslog('add and remove test successfully')
 
 driver = login('standard_user', 'secret_sauce')
